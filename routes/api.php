53b635e94b997a6814a7025b86a3fb9c895a3f63
Andrea Porcarelli <poandrea86@gmail.com>
2026-02-19 17:17:51 +0100

    Commit

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\SyncController;
 use App\Http\Middleware\SyncTokenAuth;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('sync')->middleware(SyncTokenAuth::class)->group(function () {
+Route::prefix('sync')->middleware(SyncTokenAuth::class)->withoutMiddleware('throttle:api')->group(function () {
     Route::get('/status', [SyncController::class, 'status']);
     Route::get('/export/{table}', [SyncController::class, 'export']);
     Route::get('/export-media/{mediaId}', [SyncController::class, 'exportMedia']);
