c6ac29dad317dd05ebde5bd8d36cafebb1ca6fe5
Andrea Porcarelli <poandrea86@gmail.com>
2026-04-08 12:02:50 +0200

    Cron in console

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('stock:check-low')->everySixHours();
+//Schedule::command('stock:check-low')->everySixHours();
 Schedule::command('db:backup')->cron('0 8,10,12,14,16,18,20,22,0,2,4 * * *');
 Schedule::command('mysond:op ricevute')->hourly();
