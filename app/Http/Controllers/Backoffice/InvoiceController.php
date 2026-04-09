d4c5113ba6682033ecc3d27838f4a13a2fec5b25
Andrea Porcarelli <poandrea86@gmail.com>
2026-04-09 20:07:05 +0200

    Import P7M

--- app/Http/Controllers/Backoffice/InvoiceController.php
+++ app/Http/Controllers/Backoffice/InvoiceController.php
@@ -147,7 +147,11 @@ class InvoiceController extends BaseController
                 })
                 ->addColumn('mapping', function ($item) {
                     $total     = $item->products_count;
-                    $daMappare = $item->products()->whereDoesntHave('material')->where('ignore_mapping', 0)->count();
+                    $daMappare = $item->products()->whereDoesntHave('material', function ($query) {
+                        $query->whereColumn('mapping_products.quantity_multiplier', 'supplier_invoice_products.quantity_multiplier')
+                            ->join('supplier_invoices', 'supplier_invoice_products.invoice_id', '=', 'supplier_invoices.id')
+                            ->whereColumn('mapping_products.supplier_id', 'supplier_invoices.supplier_id');
+                    })->where('ignore_mapping', 0)->count();
                     $mappati   = $item->products()->whereHas('material')->count();
                     $ignorati  = $item->products()->where('ignore_mapping', 1)->count();
 
