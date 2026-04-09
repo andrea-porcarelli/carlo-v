b96120464d57a46ebf741a62e9fb270361fe9821
Andrea Porcarelli <poandrea86@gmail.com>
2026-04-09 20:13:04 +0200

    Import P7M

--- app/Http/Controllers/Backoffice/InvoiceController.php
+++ app/Http/Controllers/Backoffice/InvoiceController.php
@@ -147,12 +147,21 @@ class InvoiceController extends BaseController
                 })
                 ->addColumn('mapping', function ($item) {
                     $total     = $item->products_count;
+                    Utils::queryLog($item->products()->whereDoesntHave('material', function ($query) {
+                        $query->whereColumn('mapping_products.quantity_multiplier', 'supplier_invoice_products.quantity_multiplier')
+                            ->join('supplier_invoices', 'supplier_invoice_products.supplier_invoice_id', '=', 'supplier_invoices.id')
+                            ->whereColumn('mapping_products.supplier_id', 'supplier_invoices.supplier_id');
+                    })->where('ignore_mapping', 0));
                     $daMappare = $item->products()->whereDoesntHave('material', function ($query) {
                         $query->whereColumn('mapping_products.quantity_multiplier', 'supplier_invoice_products.quantity_multiplier')
                             ->join('supplier_invoices', 'supplier_invoice_products.supplier_invoice_id', '=', 'supplier_invoices.id')
                             ->whereColumn('mapping_products.supplier_id', 'supplier_invoices.supplier_id');
                     })->where('ignore_mapping', 0)->count();
-                    $mappati   = $item->products()->whereHas('material')->count();
+                    $mappati   = $item->products()->whereHas('material', function ($query) {
+                        $query->whereColumn('mapping_products.quantity_multiplier', 'supplier_invoice_products.quantity_multiplier')
+                            ->join('supplier_invoices', 'supplier_invoice_products.supplier_invoice_id', '=', 'supplier_invoices.id')
+                            ->whereColumn('mapping_products.supplier_id', 'supplier_invoices.supplier_id');
+                    })->count();
                     $ignorati  = $item->products()->where('ignore_mapping', 1)->count();
 
                     $html  = '<div class="mapping-summary">';
