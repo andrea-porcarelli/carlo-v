8e54c04190927c14f5e48fb9f9fd096118915bab
Andrea Porcarelli <poandrea86@gmail.com>
2026-04-09 20:15:01 +0200

    Import P7M

--- app/Http/Controllers/Backoffice/InvoiceController.php
+++ app/Http/Controllers/Backoffice/InvoiceController.php
@@ -230,7 +230,11 @@ class InvoiceController extends BaseController
         $invoice = SupplierInvoice::findOrFail($id);
 
         $supplierInvoiceProducts = $invoice->products()
-            ->whereDoesntHave('material')
+            ->whereDoesntHave('material', function ($query) {
+                $query->whereColumn('mapping_products.quantity_multiplier', 'supplier_invoice_products.quantity_multiplier')
+                    ->join('supplier_invoices', 'supplier_invoice_products.supplier_invoice_id', '=', 'supplier_invoices.id')
+                    ->whereColumn('mapping_products.supplier_id', 'supplier_invoices.supplier_id');
+            })
             ->where('ignore_mapping', 0)
             ->orderBy('id')
             ->get();
