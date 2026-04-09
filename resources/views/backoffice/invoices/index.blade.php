d4790e4853920b5679e8c8476c12b2d2f2630ef0
Andrea Porcarelli <poandrea86@gmail.com>
2026-04-09 21:37:37 +0200

    Import P7M

--- resources/views/backoffice/invoices/index.blade.php
+++ resources/views/backoffice/invoices/index.blade.php
@@ -152,7 +152,7 @@
     </div>
     {{-- Modal Importa giacenze singola fattura --}}
     <div class="modal fade" id="importStockModal" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
