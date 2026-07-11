b9a0820ef7ecc6e77559139c77677ff342afe98a
Andrea Porcarelli <poandrea86@gmail.com>
2026-07-11 14:41:34 +0200

    Ditron Agent

--- routes/console.php
+++ routes/console.php
@@ -8,11 +8,11 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-//Schedule::command('stock:check-low')->everySixHours();
+Schedule::command('stock:check-low')->everySixHours();
 Schedule::command('db:backup')->cron('0 8,10,12,14,16,18,20,22,0,2,4 * * *');
 Schedule::command('mysond:op ricevute')->hourly();
 //Schedule::command('mysond:verifica-ade')->dailyAt('09:00');
 Schedule::command('mysond:refresh-sdi')->hourly();
-Schedule::command('ditron:close-day --source=auto')
-    ->dailyAt('23:59')
-    ->withoutOverlapping();
+//Schedule::command('ditron:close-day --source=auto')
+//    ->dailyAt('23:59')
+//    ->withoutOverlapping();
